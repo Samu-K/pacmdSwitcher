0415e8b50789e11bf9ec0bba63cd14e8f8619a77
Samu-K <samu.kaarlela@gmail.com>
2023-12-03 12:01:38 +0200

    Added application selection
    Command now takes a second parameter to specify app
    This allow source switching for other apps then spotifyd
    
    No second parameter will use spotifyd as default

--- sptSwitch.sh
+++ sptSwitch.sh
@@ -3,8 +3,9 @@
 help_text="sptSwitch is a utility to switch spotifyd audio output easily using pacmd.
 
 Usage:
-  sptSwitch sp - switch to speakers
-  sptSwitch hp - switch to headphones
+  sptSwitch <cmd> <app - optional> - switch to source given in config.
+    Given app must be in the exact name that pacmd list-sink-inputs gives
+    under application.process.binary
   sptSwitch help - display this message"
 
 spt_full_inputs=$(pacmd list-sink-inputs);
@@ -15,23 +16,36 @@ full_index=$(grep -oP 'index:\s*\K[^\s]+' <<< $spt_full_inputs);
 
 indx=1
 # set this to a unrealistic number so we can check if index was ever found
-spt_index=999
+app_index=999
 
-# search spotifyd index from pacmd
-for sic in $full_index; do 
-    src=$(echo $inputs | cut --delimiter " " --fields "$indx");
-    if [[ $src == "spotifyd" ]]; then
-        spt_index=$sic    
-    fi
-    ((indx++))
-done
+if [[ $2 == "" ]]; then    
+  # use spotifyd as default app
+  for sic in $full_index; do 
+      src=$(echo $inputs | cut --delimiter " " --fields "$indx");
+      if [[ $src == "spotifyd" ]]; then
+          app_index=$sic    
+      fi
+      ((indx++))
+  done
+else
+  # search app index from pacmd
+  for sic in $full_index; do 
+      src=$(echo $inputs | cut --delimiter " " --fields "$indx");
+      if [[ $src == $2 ]]; then
+          app_index=$sic    
+      fi
+      ((indx++))
+  done
+fi
 
 # use an array to store a command and the source 
 # this will be moved to a config file 
 declare -A src_arr;
 
-conf_path="conf.txt"
+conf_path="cmdConf.txt"
 
+# Read through conf file
+# Check conf file for more details
 while IFS= read -r line; do
   if [[ ! "$line" =~ ^# ]]; then
     IFS=',' read -r var1 var2 <<< "$line"
@@ -39,10 +53,7 @@ while IFS= read -r line; do
   fi 
 done < "$conf_path"
 
-src_arr["hp"]="UMC202HD"
-src_arr["sp"]="HD-Audio"
-
-if [[ $spt_index -eq 999 ]]; then
+if [[ $app_index -eq 999 ]]; then
     echo "Spotifyd not running" 
 else 
     full_sinks=$(pacmd list-sinks);
@@ -64,8 +75,8 @@ else
             sink_src=$(echo $sinks | cut --delimiter " " --fields "$indx");
 
             if [[ $sink_src == ${src_arr[${cmd}]} ]]; then
-              echo "Setting sink-input $spt_index to sink ${src_arr[${cmd}]} "
-              pacmd move-sink-input $spt_index $sink_id
+              echo "Setting sink-input $app_index to sink ${src_arr[${cmd}]} "
+              pacmd move-sink-input $app_index $sink_id
               fi
             ((indx++))
           done
