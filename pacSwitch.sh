1c795a706ddbcdebd505239e7bb367100d223740
Samu-K <samu.kaarlela@gmail.com>
2023-12-03 15:47:54 +0200

    Changed the name to match new features
    Improved help message
    Improved code formatting and made error messages work correctly

--- pacSwitch.sh
+++ pacSwitch.sh
@@ -1,11 +1,14 @@
 #!bin/sh
 
-help_text="sptSwitch is a utility to switch spotifyd audio output easily using pacmd.
+help_text="
+pacSwitch is a utility to switch pacmd audio from one sink to another easily.
 
 Usage:
   sptSwitch <cmd> <app - optional> - switch to source given in config.
-    Given app must be in the exact name that pacmd list-sink-inputs gives
-    under application.process.binary
+    cmd - specified in the config file
+    app - must be in the exact name that pacmd list-sink-inputs gives
+          under application.process.binary. Defaults to spotifyd
+
   sptSwitch help - display this message"
 
 spt_full_inputs=$(pacmd list-sink-inputs);
@@ -42,7 +45,7 @@ fi
 # this will be moved to a config file 
 declare -A src_arr;
 
-conf_path="cmdConf.txt"
+conf_path="/home/samuk/Desktop/coding/pacSwitch/cmdConf.txt"
 
 # Read through conf file
 # Check conf file for more details
@@ -53,34 +56,31 @@ while IFS= read -r line; do
   fi 
 done < "$conf_path"
 
-if [[ $app_index -eq 999 ]]; then
-    echo "Spotifyd not running" 
+if [[ $1 == "" ]]; then
+    echo "No parameter given. Use sptSwitch help to display help."
+elif [[ $1 == "help" ]]; then
+  echo "$help_text"
+elif [[ $app_index -eq 999 ]]; then
+    echo "$2 not found or not running" 
 else 
     full_sinks=$(pacmd list-sinks);
 
     sinks=$(grep -oP 'alsa.card_name = "\K[^"]\S+' <<< $full_sinks);
     full_sink_index=$(grep -oP 'index:\s*\K[^\s]+' <<< $full_sinks);
+    indx=1
+  
+    for cmd in ${!src_arr[@]}; do
+      if [[ $1 == $cmd ]]; then 
+        # we need to find the correct id for given sink
+        for sink_id in $full_sink_index; do
+          sink_src=$(echo $sinks | cut --delimiter " " --fields "$indx");
 
-    if [[ $1 == "" ]]; then
-      echo "No parameter given. Use sptSwitch help to display help."
-    elif [[ $1 == "help" ]]; then
-      echo "$help_text"
-    else
-      indx=1
-    
-      for cmd in ${!src_arr[@]}; do
-        if [[ $1 == $cmd ]]; then 
-          # we need to find the correct id for given sink
-          for sink_id in $full_sink_index; do
-            sink_src=$(echo $sinks | cut --delimiter " " --fields "$indx");
-
-            if [[ $sink_src == ${src_arr[${cmd}]} ]]; then
-              echo "Setting sink-input $app_index to sink ${src_arr[${cmd}]} "
-              pacmd move-sink-input $app_index $sink_id
-              fi
-            ((indx++))
-          done
-        fi
-      done
-    fi
+          if [[ $sink_src == ${src_arr[${cmd}]} ]]; then
+            echo "Setting sink-input $app_index to sink ${src_arr[${cmd}]} "
+            pacmd move-sink-input $app_index $sink_id
+            fi
+          ((indx++))
+        done
+      fi
+    done
 fi
